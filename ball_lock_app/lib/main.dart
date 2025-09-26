beb05e6f6de1fff970a5a395d20c076f4cb2a009
quietjun802 <whdydwns802@gmail.com>
2025-09-26 16:20:06 +0900

    1

--- ball_lock_app/lib/main.dart
+++ ball_lock_app/lib/main.dart
@@ -3,13 +3,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart'; // ✅ Provider 추가
 import 'firebase_options.dart';
 import 'screens/splash_screen.dart';
-<<<<<<< HEAD
+
 import 'theme/app_theme.dart';
 import 'theme/theme_provider.dart'; // ✅ ThemeProvider import
-=======
-import 'screens/home_screen.dart';
-import 'services/db_upload.dart'; // ✅ 우리가 만든 DB 업로드 파일
->>>>>>> kth
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,19 +17,12 @@ Future<void> main() async {
     );
   }
 
-<<<<<<< HEAD
   runApp(
     ChangeNotifierProvider(
       create: (_) => ThemeProvider(),
       child: const MyApp(),
     ),
   );
-=======
-  // ✅ DB 업로드 실행 (임시)
-  await uploadSuwonData();
-
-  runApp(const MyApp());
->>>>>>> kth
 }
 
 class MyApp extends StatelessWidget {
